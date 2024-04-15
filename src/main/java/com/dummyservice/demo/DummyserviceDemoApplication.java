3d93f6fced416d4d6b513a2c9d71f28405527529
nikhilballari <nikhil.ballari@gmail.com>
2024-04-15 13:11:20 +0100

    1st commit: Uploading the project ot github

---
+++ src/main/java/com/dummyservice/demo/DummyserviceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dummyservice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyserviceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyserviceDemoApplication.class, args);
+	}
+
+}
